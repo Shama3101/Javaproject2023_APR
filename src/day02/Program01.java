3e96b1ad144d8a6c6f98437ab43fdd32e1e6fb23
Shama3101 <shamaparween4096@gmail.com>
2023-04-07 21:37:58 +0530

    Day02
    Added one  program and details of the data table

---
+++ src/day02/Program01.java
@@ -0,0 +1,39 @@
+package day02;
+
+public class Program01 {
+
+	public static void main(String[] args) {
+		boolean a = true;
+		System.out.println(a);
+		
+		byte a1= -128;
+		System.out.println(a1);
+		
+		short a2=32767;
+		System.out.println(a2);
+		
+		int a3=234567;
+		System.out.println(a3);
+		
+		long a4=23456780988l;
+		System.out.println(a4);
+		
+		float a5=237464.5858f;
+		System.out.print(a5);
+		
+		double a6=454645364.947446d;
+		System.out.print(a6);
+		
+		char a7='r';
+		System.out.println(a7);
+		
+		String a8= "welcome";
+		System.out.println(a8);
+		
+		
+	
+		
+
+	}
+
+}
