0fa84ccd9acf865f5d3af99d91f48dedaafe3df2
Shama3101 <shamaparween4096@gmail.com>
2023-04-10 20:38:44 +0530

    day03
    Static non-static, add and subtract function, return

---
+++ src/day03/Program01.java
@@ -0,0 +1,23 @@
+package day03;
+
+public class Program01 {
+
+	public static void main(String[] args) {
+		addFunction(10,20);
+		addFunction(20,30);
+	}
+
+		
+		
+		
+public static void addFunction(int a,int b) {
+
+
+	System.out.println(a+b);
+}
+		
+		
+		
+	}
+
+
---
+++ src/day03/Program02.java
@@ -0,0 +1,31 @@
+package day03;
+
+public class Program02 {
+
+	public static void main(String[] args) {
+		addFunction(20,40);
+		Program02 n=new Program02();
+		n.subFunction(30,60);
+		
+		
+	}
+
+		
+		
+		
+		public static void addFunction(int a,int b) {
+		
+		
+			System.out.println(a+b);
+			
+		}
+		public void subFunction(int a,int b) {
+		
+			System.out.println(a-b);
+		}
+		
+		
+		
+	}
+
+
